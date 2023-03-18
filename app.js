7abdcaf5eeb286d34f7dd96ea1e037c4f80850ab
ankitsoni4448 <56527089+ankitsoni4448@users.noreply.github.com>
2023-03-18 12:59:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
 const inputEl = document.getElementById("input")
+const infoTextEl = document.getElementById("info")
+const meaningContainerEl = document.getElementById("meaning-container");
+const titleEl = document.getElementById("title")
+const meaningEl = document.getElementById("meaning")
+const audioEl = document.getElementById("audio")
 
-
-function fetchAPI(word){
+async function fetchAPI(word){
    // console.log(word);
-   const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
-   const result = fetch(url).then((res) => res.json());
-   console.log(result);
+try {
+    infoTextEl.style.display = "block"; 
+    meaningContainerEl.style.display = "none";
+    infoTextEl.innerText = `Searching the meaning of "${word}"`
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    const result = await fetch(url).then((res) => res.json());
+    
+    if(result.title){
+        meaningContainerEl.style.display = "block";
+        infoTextEl.style.display = "none"
+        titleEl.innerText = word;
+    meaningEl.innerText = "N/A";
+    audioEl.style.display = "none";
+    }
+    else{
+        infoTextEl.style.display = "none"
+    meaningContainerEl.style.display = "block";
+    audioEl.style.display = "inline-flex"
+    titleEl.innerText = result[0].word
+    meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+    audioEl.src = result[0].phonetics[0].audio;
+
+    }
+
+    /*infoTextEl.style.display = "none"
+    meaningContainerEl.style.display = "block";
+    titleEl.innerText = result[0].word
+    meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+    audioEl.src = result[0].phonetics[0].audio;*/
+
+} catch (error) {
+    console.log(error);
+    infoTextEl.innerText = `An error happened try again later`
+}
 }
 
 
 inputEl.addEventListener("keyup",(e)=>{
 //console.log(e.target.value);  chaching the value 
 if(e.target.value && e.key === "Enter"){
-    fetchAPI(e.target.value)
+    fetchAPI(e.target.value);
 }
-})
+});
